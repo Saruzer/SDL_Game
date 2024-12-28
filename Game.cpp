663c421265a15e62e4d1781b567222600482be17
Saruzer Saruzer <mrlexa130@gmail.com>
2024-12-28 21:42:07 +0200

    Added image SDL and player texture

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,8 @@
 #include "Game.h"
 
+SDL_Texture* playerTex;
+SDL_Rect srcR, destR;
+
 Game::Game()
 {
 }
@@ -20,6 +23,11 @@ void Game::Init(const char* title, int x, int y, int width, int height, bool ful
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 		isRunning = true;
 	}
+
+	SDL_Surface* tmpSurface = IMG_Load("Zombie.png");
+	playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
+
 }
 
 void Game::HandleEvents()
@@ -33,14 +41,20 @@ void Game::HandleEvents()
 		break;
 	}
 }
-
+int cnt = 0;
 void Game::Update()
 {
+	destR.w = 159;
+	destR.h = 256;
+	destR.x = cnt;
+	cnt++;
 }
 
 void Game::Render()
 {
 	SDL_RenderClear(renderer);
+
+	SDL_RenderCopy(renderer, playerTex, 0, &destR);
 	SDL_RenderPresent(renderer);
 }
 
--- SDL_Game.cpp
+++ SDL_Game.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 #include "Game.h"
 
+
 Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+	
+	Uint32 frameStart;
+	int frameTime;
+	
 	game = new Game();
-	game->Init("Simple game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, 0);
+	game->Init("Simple game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, 0);
+	
 	while (game->Running()) {
 		
+		frameStart = SDL_GetTicks();
+		
+
 		game->HandleEvents();
 		game->Update();
 		game->Render();
 	
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+
+		}
+
 	}
 	game->Clean();
 	return 0;
